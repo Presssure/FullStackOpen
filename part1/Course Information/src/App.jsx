b9d13ee84f3664fe9c5e952706ba4ff615246444
Benny Yang <bennyyang179@gmail.com>
2024-05-13 07:16:43 +1000

    part1b completed

--- part1/Course Information/src/App.jsx
+++ part1/Course Information/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Content = (props) => {
+  console.log(props.parts.parts[0].name, props.parts.parts[0].exercises);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {props.parts.parts[0].name} {props.parts.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts.parts[1].name} {props.parts.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts.parts[2].name} {props.parts.parts[2].exercises}
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+const Footer = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.parts[0].exercises +
+        props.parts.parts[1].exercises +
+        props.parts.parts[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course} />
+      <Footer parts={course} />
+    </div>
+  );
+};
+
+export default App;
