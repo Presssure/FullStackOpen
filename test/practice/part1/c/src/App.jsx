04e71d5a720897e31f80612e0f8c237a14447c35
BennyYang <bennyyang179@gmail.com>
2025-07-04 06:26:37 +1000

    worked on test/practice/part1 d materials

--- test/practice/part1/c/src/App.jsx
+++ test/practice/part1/c/src/App.jsx
@@ -1,15 +1,15 @@
 const Hello = ({ name, age }) => {
-
-  const bornYear = () => new Date().getFullYear() - age
+  const bornYear = () => new Date().getFullYear() - age;
 
   return (
     <div>
-
-      <p>Hello {name}, you are {age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
-  )
-}
+  );
+};
 
 // const App = () => {
 //   const name = 'Peter'
@@ -24,30 +24,51 @@ const Hello = ({ name, age }) => {
 //   )
 // }
 
-import { useState } from 'react'
+import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        <p>the app is used by pressing the buttons</p>
+      </div>
+    );
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+    setTotal(total + 1);
+  };
 
-  const setToZero = () => setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+    setTotal(total + 1);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='plus' />
-      <Button onClick={decreaseByOne} text='minus' />
-      <Button onClick={setToZero} text='reset' />
-    </div >
-  )
-}
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
 
-export default App
+export default App;
